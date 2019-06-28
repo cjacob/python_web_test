0e607318999786281b4c579886de4f548e3ca5fd
cjacob <christian.jacob@80-20.de>
2019-06-28 19:48:49 +0200

    Update application.py

--- application.py
+++ application.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+  
+class Employees(Resource):
+    def get(self):
+
+        return {1,1,1,2,3,3,4,3} # Fetches first column that is Employee ID
+      
+api.add_resource(Employees, '/employees') # Route_1
